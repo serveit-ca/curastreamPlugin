79c9d624b4e87cd7d5904ef0992c86c5dcc7c569
Kaiden McFayden-Faint <kaiden@ServeIT005.local>
2018-11-14 10:53:32 -0800

    Testing

--- testing.php
+++ testing.php
@@ -3,13 +3,35 @@
 require_once ("objects/program.php");
 require_once ("objects/phase.php");
 require_once ("objects/exercise.php");
-$programs = new program();
 
-$active = $programs->getAllActiveGenericPrograms();
+$newProg = $programs->createProgram("Testphp Prog1");
+$newPhaseId = $programs->createPhase("New Phase1", $newProg);
+$highestOrder = $programs->getHighestPhaseOrder($newProg);
+$programs->updatePhase(NULL, NULL, NULL, NULL, $highestOrder, $newPhaseId);
 // Order the Phase to the final order 
-print_r($active);
+$programs->movePhaseOrder($newProg,$newPhaseId,$highestOrder,1);
+
+// New Phase
+ $newPhaseId = $programs->createPhase("New Phase3", $newProg);
+$highestOrder = $programs->getHighestPhaseOrder($newProg);
+$programs->updatePhase(NULL, NULL, NULL, NULL, $highestOrder, $newPhaseId);
+// Order the Phase to the final order 
+$programs->movePhaseOrder($newProg,$newPhaseId,$highestOrder,2);
 
 
+// New Phase
+ $newPhaseId = $programs->createPhase("New Phase4", $newProg);
+$highestOrder = $programs->getHighestPhaseOrder($newProg);
+$programs->updatePhase(NULL, NULL, NULL, NULL, $highestOrder, $newPhaseId);
+// Order the Phase to the final order 
+$programs->movePhaseOrder($newProg,$newPhaseId,$highestOrder,3);
+
+// New Phase
+ $newPhaseId = $programs->createPhase("New Phase2", $newProg);
+$highestOrder = $programs->getHighestPhaseOrder($newProg);
+$programs->updatePhase(NULL, NULL, NULL, NULL, $highestOrder, $newPhaseId);
+// Order the Phase to the final order 
+$programs->movePhaseOrder($newProg,$newPhaseId,$highestOrder,2);
 
 ?>
 
