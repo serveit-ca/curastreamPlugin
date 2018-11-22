993578768ddd2113be51e1f0aa14df54e3482c1b
Kaiden McFayden-Faint <kaiden@ServeIT005.local>
2018-11-21 16:31:28 -0800

    Travis Test 7

---
+++ tests/test-wp-program.php
@@ -0,0 +1,23 @@
+<?php 
+
+class WP_Program_Test extends WP_UnitTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->class_instance = new WP_Program_Test();
+    }
+
+    public function test_google_site_verification()
+    {
+    	//placeholder function
+    }
+
+    public function test_bing_site_verification()
+    {
+    	//placeholder fucntion
+    }
+}
+
+?>
