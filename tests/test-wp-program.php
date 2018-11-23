dd8354ec7317521480660601fefa4f77bfafd1cb
Kaiden McFayden-Faint <kaiden@ServeIT005.local>
2018-11-23 11:02:59 -0800

    Travis Test 29

--- tests/test-wp-program.php
+++ tests/test-wp-program.php
@@ -9,13 +9,22 @@ class WP_Program_Test extends WP_UnitTestCase
         $this->class_instance = new WP_Program_Test();
     }
 
+    public function reset_database(){
+    	$command = "mysql --user={$vals['root']} --password='{$vals['']}' "
+ 		. "-h {$vals['db_host']} -D {$vals['db_name']} < {'travis.sql'}";
+
+		$output = shell_exec($command . '/shellexec.sql');
+    }
+
     public function test_get_program_by_id()
     {
-
     	$programs = new program();
     	$program37 = $programs->getProgramById(37);
-    	assert($program37->id == 35, "getProgramById");
+    	assert($program37->id == 37, "getProgramById");
+    	$this->reset_database();
     }
+
+
 
 
 }
