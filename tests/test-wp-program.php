8fb3569dadab07a2db8ff0d24921b2d43ae0c166
Kaiden McFayden-Faint <kaiden@ServeIT005.local>
2018-11-30 15:19:52 -0800

    Travis Test 118

--- tests/test-wp-program.php
+++ tests/test-wp-program.php
@@ -309,6 +309,18 @@ class WP_Program_Test extends WP_UnitTestCase
    	assert($phaseOne->order_no == 1);
    	assert($phaseTwo->order_no == 2);
    	assert($phaseFour->order_no == 4);
+   	// change phase order of 1 to 2
+   	$programs->movePhaseOrder($newProgId, $phaseOneId, 1, 2);
+   	$phaseOne = $programs->getAPhaseById($phaseOneId);
+   	$phaseTwo = $programs->getAPhaseById($phaseTwoId);
+   	assert($phaseOne->order_no == 2);
+   	assert($phaseTwo->order_no == 1);
+   	// change phase order of 2 to 1
+   	$programs->movePhaseOrder($newProgId, $phaseOneId, 2, 1);
+   	$phaseOne = $programs->getAPhaseById($phaseOneId);
+   	$phaseTwo = $programs->getAPhaseById($phaseTwoId);
+   	assert($phaseOne->order_no == 1);
+   	assert($phaseTwo->order_no == 2);
    	//Remove a Phase from the end of the program
    	$programs->deletePhaseUpdateOrder($newProgId, $phaseFourId, 4);
    	$programs = new program();
