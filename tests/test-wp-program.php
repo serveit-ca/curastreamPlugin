b72d3255e985a8850aabb79306588216a2d35e9d
Kaiden McFayden-Faint <kaiden@ServeIT005.local>
2018-12-03 11:19:50 -0800

    Travis Test 124

--- tests/test-wp-program.php
+++ tests/test-wp-program.php
@@ -395,36 +395,36 @@ class WP_Program_Test extends WP_UnitTestCase
 	$highestOrder = $programs->getHighestExerciseOrder($newPhaseId);
 	$programs->updateExercise($highestOrder+1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, $exerciseOneId);
 	$programs = new program();
-	$exerciseOne = $programs->getAExerciseById($exerciseOneId);
+	$exerciseOne = $programs->getAnExerciseById($exerciseOneId);
 	assert($exerciseOne->order_no == 1);
 	//add a second exercise to a phase with one exercise to the end
 	$exerciseTwoId = $programs->createExercise("Test Exercise 2 for Move Exercise", $newPhaseId);
 	$highestOrder = $programs->getHighestExerciseOrder($newPhaseId);
 	$programs->updateExercise($highestOrder+1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, $exerciseTwoId);
 	$programs = new program();
-	$exerciseTwo = $programs->getAExerciseById($exerciseTwoId);
+	$exerciseTwo = $programs->getAnExerciseById($exerciseTwoId);
 	assert($exerciseTwo->order_no == 2);
 	//add a third exercise to the first position of the phase - Ensure exercise 1 becomes order 2 and exercise 2 becomes order 3
 	$exerciseThreeId = $programs->createExercise("Test Exercise 3 for Move Exercise", $newPhaseId);
 	$highestOrder = $programs->getHighestExerciseOrder($newPhaseId);
 	$programs->updateExercise($highestOrder+1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, $exerciseTwoId);
 	$programs = new program();
-	$exerciseThree = $programs->getAExerciseById($exerciseThreeId);
+	$exerciseThree = $programs->getAnExerciseById($exerciseThreeId);
 	assert($exerciseThree->order_no == 3);
 	$programs->moveExerciseOrder($newPhaseId, $exerciseThreeId, 3, 1);
 	$programs = new program();
-	$exerciseThree = $programs-> getAExerciseById($exerciseThreeId);
-	$exerciseOne = $programs-> getAExerciseById($exerciseOneId);
-	$exerciseTwo = $programs-> getAExerciseById($exerciseTwoId);
+	$exerciseThree = $programs-> getAnExerciseById($exerciseThreeId);
+	$exerciseOne = $programs-> getAnExerciseById($exerciseOneId);
+	$exerciseTwo = $programs-> getAnExerciseById($exerciseTwoId);
 	assert($exerciseThree->order_no == 1);
 	assert($exerciseTwo->order_no == 3);
 	assert($exerciseOne->order_no == 2);
 	//Move Back
 	$programs->moveExerciseOrder($newPhaseId, $exerciseThreeId, 1, 3);
 	$programs = new program();
-	$exerciseThree = $programs-> getAExerciseById($exerciseThreeId);
-	$exerciseOne = $programs-> getAExerciseById($exerciseOneId);
-	$exerciseTwo = $programs-> getAExerciseById($exerciseTwoId);
+	$exerciseThree = $programs-> getAnExerciseById($exerciseThreeId);
+	$exerciseOne = $programs-> getAnExerciseById($exerciseOneId);
+	$exerciseTwo = $programs-> getAnExerciseById($exerciseTwoId);
 	assert($exerciseThree->order_no == 3);
 	assert($exerciseTwo->order_no == 2);
 	assert($exerciseOne->order_no == 1);
