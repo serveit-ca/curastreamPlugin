b6bbcf6b562c8bd7a75286beea8d6fd3c3ae68bb
Kaiden McFayden-Faint <kaiden@ServeIT005.local>
2018-12-04 09:13:02 -0800

    Travis Test 134

--- testing.php
+++ testing.php
@@ -13,23 +13,43 @@ $programs = new program();
 	$programs->updateExercise($highestOrder+1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, $exerciseOneId);
 	$programs = new program();
 	$exerciseOne = $programs->getAnExerciseById($exerciseOneId);
-	//assert($exerciseOne->order_no == 1);
+	assert($exerciseOne->order_no == 1);
 	//add a second exercise to a phase with one exercise to the end
 	$exerciseTwoId = $programs->createExerciseByName("Test Exercise 2 for Move Exercise", $newPhaseId);
 	$highestOrder = $programs->getHighestExerciseOrder($newPhaseId);
 	$programs->updateExercise($highestOrder+1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, $exerciseTwoId);
 	$programs = new program();
 	$exerciseTwo = $programs->getAnExerciseById($exerciseTwoId);
-	//assert($exerciseTwo->order_no == 2);
+	assert($exerciseTwo->order_no == 2);
 	//add a third exercise to the first position of the phase - Ensure exercise 1 becomes order 2 and exercise 2 becomes order 3
 	$exerciseThreeId = $programs->createExerciseByName("Test Exercise 3 for Move Exercise", $newPhaseId);
 	$highestOrder = $programs->getHighestExerciseOrder($newPhaseId);
 	$programs->updateExercise($highestOrder+1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, $exerciseThreeId);
 	$programs = new program();
 	$exerciseThree = $programs->getAnExerciseById($exerciseThreeId);
-	//assert($exerciseThree->order_no == 3);
+	assert($exerciseThree->order_no == 3);
+	//add 4th for further tests
+	$exerciseFourId = $programs->createExerciseByName("Test Exercise 4 for Move Exercise", $newPhaseId);
+	$highestOrder = $programs->getHighestExerciseOrder($newPhaseId);
+	$programs->updateExercise($highestOrder+1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4, $exerciseFourId);
+	$programs = new program();
+	$exerciseFour = $programs->getAnExerciseById($exerciseFourId);
+	assert($exerciseFour->order_no == 4);
+	
+   		//remove exercise from end of phase
+   	$programs->deleteExerciseUpdateOrder($newPhaseId, $exerciseFourId, 4);
+   	$programs = new program();
+   	$allExercises = $programs->getExercisesByPhaseId($newPhaseId);
+   	$expectedIds = array($exerciseOneId,$exerciseTwoId,$exerciseThreeId);
+   	$i = 0;
+   	foreach($allExercises as $key){
+   		echo $i;
+   		assert($key->id == $expectedIds[$i]);
+   		$i++;
 
-	print_r($exerciseThree);
+   }
+
+	
    
 
 //Testing for Move Exercise/Phase Code
