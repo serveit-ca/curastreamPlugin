a0d4141aa683434e375ab1b8b3eb2d18c943e68b
serveit-ca <nathan@serveit.ca>
2019-04-02 21:37:03 -0700

    BodyPartsUpdate

--- tests/test-wp-program.php
+++ tests/test-wp-program.php
@@ -699,7 +699,7 @@ public function test_move_phase_order(){
       $count++;
     }
   }  
-1 knee 15 Shoulder, 5 elbow 13 Ankle Foot
+
 public function test_get_Body_Part_Names_By_Ids(){
     $programs = new program();
     $body_parts = $programs->getBodyPartNamesByIds(1,15,5,13);
